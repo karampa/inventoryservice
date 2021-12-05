f93c308c9dda2c87544fb0e21e6e43504829b05b
PK7 <karam@bluewin.ch>
2021-12-05 17:08:33 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/karampa/inventoryservice
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Product struct {
+	ProductID      int    `json:"productId"`
+	Manufacturer   string `json:"manufacturer"`
+	Sku            string `json:"sku"`
+	Upc            string `json:"upc"`
+	PricePerUnit   string `json:"pricePerUnit"`
+	QuantityOnHand int    `json:"quantityOnHand"`
+	ProductName    string `json:"productname"`
+}
+
+var productList []Product
+
+func init() {
+	productJson := `[
+		{
+			"productId": 1,
+			"manufacturer": "Johns-Jenkins",
+			"sku": "p5z343vdS",
+			"upc": "939581000000",
+			"pricePerUnit": "497.45",
+			"quantityOnHand": 9703,
+			"productName": "sticky note"
+		},
+		{
+			"productId": 2,
+			"manufacturer": "Hessel, Schimmel and Feeney",
+			"sku": "i7v300kmx",
+			"upc": "740979000000",
+			"pricePerUnit": "282.29",
+			"quantityOnHand": 9217,
+			"productName": "leg warmers"
+		},
+		{
+			"productId": 3,
+			"manufacturer": "Swaniawski, Bartoletti and Bruen",
+			"sku": "q0L657ys7",
+			"upc": "111730000000",
+			"pricePerUnit": "436.26",
+			"quantityOnHand": 5905,
+			"productName": "lamp shade"
+		}
+	]`
+
+	err := json.Unmarshal([]byte(productJson), &productList)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getNextProductID() int {
+	highestID := -1
+	for _, product := range productList {
+		if highestID < product.ProductID {
+			highestID = product.ProductID
+		}
+	}
+	return highestID + 1
+}
+
+func productHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		productJson, err := json.Marshal(productList)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(productJson)
+	case http.MethodPost:
+		var newProduct Product
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		err = json.Unmarshal(bodyBytes, &newProduct)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if newProduct.ProductID != 0 {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		newProduct.ProductID = getNextProductID()
+
+		productList = append(productList, newProduct)
+		w.WriteHeader(http.StatusCreated)
+		return
+	}
+
+}
+
+func main() {
+	http.HandleFunc("/products", productHandler)
+	http.ListenAndServe(":5000", nil)
+}
